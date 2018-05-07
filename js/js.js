d365b66348795f2392f1c5d97fcd7da9a75abda5
suchenxiao <suchenxiao@foxmail.com>
2018-05-07 21:50:54 +0800

    Refactor: package markers

--- js/js.js
+++ js/js.js
@@ -1,34 +1,79 @@
 // 地图变量
 var map;
-// 储备标记列表
-var markers = [];
+
+// 标记变量
+var Mark = {
+	
+  init : function() {
+    // 储备标记列表
+    this.markers = [];
+	
+    // 设置标记默认样式
+	this.defaultIcon = this.makeMarkerIcon('0091ff');
+	
+	// 设置标记焦点样式
+	this.highlightedIcon =  this.makeMarkerIcon('ffff24');
+	
+  },
+    
+  // 显示标记列表，并调整地图可视范围
+  showMarkers : function() {
+	var bounds = new google.maps.LatLngBounds();
+	this.markers.forEach(function(marker) {
+	  marker.setMap(map);
+	  bounds.extend(marker.position);
+	});
+	map.fitBounds(bounds);
+  },
+  
+  // 隐藏标记列表
+  hideMarkers : function(markers) {
+	this.markers.forEach(function(marker) {
+	  marker.setMap(null);
+	});
+  },
+	  
+  // 绘制标记样式
+  makeMarkerIcon : function(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+	  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+	  '|40|_|%E2%80%A2',
+	  new google.maps.Size(21, 34),
+	  new google.maps.Point(0, 0),
+	  new google.maps.Point(10, 34),
+	  new google.maps.Size(21,34));
+	return markerImage;
+  },
+  
+}
+
 // 初始地点
 var initialLocations = [
-	{
-	  index : 0,
-	  title : '故宫博物院',
-	  location: {lat: 39.923841, lng: 116.389809}
-	},
-	{
-	  index : 1,
-	  title : '西单图书大厦',
-	  location: {lat: 39.919826, lng: 116.394788}
-	},
-	{
-	  index : 2,
-	  title : '天安门',
-	  location: {lat: 39.919826, lng: 116.394788}
-	},
-	{
-	  index : 3,
-	  title : '王府井步行街',
-	  location: {lat: 39.9105551, lng: 116.4103644}
-	},
-	{
-	  index : 4,
-	  title : '景山公园',
-	  location: {lat: 39.9241704, lng: 116.3921251}
-	}
+  {
+	index : 0,
+	title : '故宫博物院',
+	location: {lat: 39.923841, lng: 116.389809}
+  },
+  {
+	index : 1,
+	title : '西单图书大厦',
+	location: {lat: 39.919826, lng: 116.394788}
+  },
+  {
+	index : 2,
+	title : '天安门',
+	location: {lat: 39.919826, lng: 116.394788}
+  },
+  {
+	index : 3,
+	title : '王府井步行街',
+	location: {lat: 39.9105551, lng: 116.4103644}
+  },
+  {
+	index : 4,
+	title : '景山公园',
+	location: {lat: 39.9241704, lng: 116.3921251}
+  }
 ];
 
 // 初始化
@@ -38,16 +83,15 @@ function initMap() {
 	center: {lat: 39.929074, lng: 116.310931},
 	zoom: 13
   });
+  Mark.init();
+  
   // 加载搜索框
   var searchBox = new google.maps.places.SearchBox(document.getElementById('places-search'));
   // 绑定搜索框
   searchBox.setBounds(map.getBounds());
   // 加载信息窗口
   var largeInfowindow = new google.maps.InfoWindow();
-  // 设置标记默认样式
-  var defaultIcon = makeMarkerIcon('0091ff');
-  // 设置标记焦点样式
-  var highlightedIcon = makeMarkerIcon('FFFF24');
+  
 		
   // 为初始地点绘制标记
   initialLocations.forEach(function(locationItem){
@@ -58,23 +102,23 @@ function initMap() {
 	  position: position,
 	  title: title,
 	  animation: google.maps.Animation.DROP,
-	  icon: defaultIcon,
+	  icon: Mark.defaultIcon,
 	  id: id
 	});
 	marker.addListener('click', function() {
 	  //populateInfoWindow(this, largeInfowindow);
 	});
 	marker.addListener('mouseover', function() {
-	  this.setIcon(highlightedIcon);
+	  this.setIcon(Mark.highlightedIcon);
 	});
 	marker.addListener('mouseout', function() {
-	  this.setIcon(defaultIcon);
+	  this.setIcon(Mark.defaultIcon);
 	});
 	// 放入markers列表
-	markers.push(marker);
+	Mark.markers.push(marker);
   });
 			
-  // 加载UI
+  // 加载非地图区域UI
   (function() {
 	var Location = function(data) {
 	  this.title = ko.observable(data.title);
@@ -95,37 +139,7 @@ function initMap() {
 	};
 	
 	ko.applyBindings(new ViewModel());
-	showMarkers(markers);
+	Mark.showMarkers();
   })()
   
-}
-
-
-// 显示标记列表，并调整地图可视范围
-function showMarkers(markers) {
-  var bounds = new google.maps.LatLngBounds();
-  for (var i = 0; i < markers.length; i++) {
-	markers[i].setMap(map);
-	bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-}
-
-// 隐藏标记列表
-function hideMarkers(markers) {
-  for (var i = 0; i < markers.length; i++) {
-	markers[i].setMap(null);
-  }
-}
-	  
-// 绘制标记样式
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-	'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-	'|40|_|%E2%80%A2',
-	new google.maps.Size(21, 34),
-	new google.maps.Point(0, 0),
-	new google.maps.Point(10, 34),
-	new google.maps.Size(21,34));
-  return markerImage;
 }
